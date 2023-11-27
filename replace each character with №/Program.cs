724cb6d63c39e4ea36081927389c0e450e5403dc
KP_349 <KP_349@FANARIER>
2023-11-27 16:31:23 +0200

    Добавьте файлы проекта.

---
+++ replace each character with №/Program.cs
@@ -0,0 +1,44 @@
+﻿//22-ИСП-3/2 Плотников Кирилл
+//В заданной строке заменить каждый символ «№» строкой «номер по порядку».
+//Я заменил все "#" вместо "№" потому что так визуально удобнее.
+
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите предложение:");
+        string input = Console.ReadLine();
+        string output = ReplaceNumberInString(input);
+        Console.WriteLine(output);
+    }
+
+    static string ReplaceNumberInString(string input)
+    {
+        int counter = 0;
+        string output = "";
+
+        Thread thread = new Thread(() =>
+        {
+            foreach (char c in input)
+            {
+                if (c == '#')
+                {
+                    counter++;
+                    output += counter.ToString();
+                }
+                else
+                {
+                    output += c;
+                }
+            }
+        });
+
+        thread.Start(); 
+        thread.Join();  
+
+        return output;
+    }
+}
